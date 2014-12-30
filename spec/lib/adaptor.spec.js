452507f50413b00469238d927ea268df1493fd8c
Loren West <github@lorenwest.com>
2014-12-29 23:44:44 -0800

    Changed adaptor test from integration to unit

--- spec/lib/adaptor.spec.js
+++ spec/lib/adaptor.spec.js
@@ -1,33 +1,21 @@
 'use strict';
 
 var Cylon = require('cylon');
-var RobotDisco = source("adaptor");
-Cylon.config({
-  logging: {level: 'warn'},
-  api: {port:3000, ssl: false}
-})
-
-// The API must be running in order to be discoverable
-Cylon.api();
-
-// Define a robot with this adaptor
-var robot = Cylon.robot({
-  name: 'cylon-disco-testbot',
-  connections: {
-    disco: {adaptor: 'robot-disco', port: 12345, secret:'hand-shake'}
-  }
-});
-var adaptor = robot.connections.disco;
+var StubAdaptor = source("adaptor");
 
 describe("Cylon.Adaptors.RobotDisco", function() {
+  var adaptor = new StubAdaptor();
 
   it("has an adaptor", function() {
     expect(adaptor).to.be.instanceOf(Cylon.Adaptor);
   });
 
-  it('starts up nicely', function(done) {
-    robot.start(done);
-  })
+  it("Calls the connect callback", function(done) {
+    adaptor.connect(done);
+  });
 
+  it("Calls the disconnect callback", function(done) {
+    adaptor.disconnect(done);
+  });
 
 });
