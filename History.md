f601a53dd944a83dad84bc13ad165510950a258c
Loren West <open_source@lorenwest.com>
2015-04-24 19:40:35 -0700

    Made compatible w/cylon 1.0

--- History.md
+++ History.md
@@ -1,3 +1,10 @@
+1.0.0 / 2015-04-24
+==================
+
+  * Made cylon-robot-disco compatible w/cylon 1.0
+  * Verified compatibility using spec/testbot.js
+  * Bumped up the compatibility version to 1.0
+
 0.1.4 / 2015-02-20
 ==================
 
--- spec/testbot.js
+++ spec/testbot.js
@@ -2,17 +2,26 @@
 
 // Minimum robot - for testing
 // usage: testbot {cylon-api-port} [secret]
+
+// Run 2 or 3 of these at the same time - they run on different ports.
+// Connect your browser to any one of them and you should see all the
+// robots from any robot. You should be able to interact with the local
+// robot and any of the remote robots from the same UI.
+
 var apiPort = +process.argv[2] || 3000 + Math.round((Math.random() * 1000));
 var secret = process.argv[3] || null;
 
 var Cylon = require('cylon');
 Cylon.config({
   logging: {level:'debug'},
-  api: {port:apiPort, host:'0.0.0.0', ssl: false}
 })
 
 // The API must be running in order to be discoverable
-Cylon.api();
+Cylon.api({
+    port:apiPort,
+    host:'0.0.0.0',
+    ssl: false
+});
 
 // Define a robot with this adaptor
 var robot = Cylon.robot({
